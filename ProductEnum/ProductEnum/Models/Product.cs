734b3f19ab4eb6649ac00a204e31bdca91938c5e
ENRIQUE NUÑEZ CESPEDES <a2019112540@estudiantes.upsa.edu.bo>
2020-09-12 11:57:50 -0400

    Controlador creado

---
+++ ProductEnum/ProductEnum/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductEnum.Models
+{
+    public enum CategoryType
+    {
+        Electronicos = 10,
+        Alimenticios = 20,
+        Moda = 30,
+        Belleza = 40,
+        Automotriz = 50,
+        Libros = 60
+    };
+    public class Product
+    {
+        [Key]
+        public int productID { get; set; }
+        [Display(Name = "Nombre del producto")]
+        [Required(ErrorMessage = "Debe ingresar el nombre del producto")]
+        public string name { get; set; }
+        [Display(Name = "Categoria del producto")]
+        [Required(ErrorMessage = "Debe ingresar la categoria del producto")]
+        public CategoryType category { get; set; }
+    }
+}
